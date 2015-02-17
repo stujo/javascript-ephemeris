fee22ce3f0e11fb5a7c1e86c80f8a5a67d2de927
stujo <stujo@users.noreply.github.com>
2015-02-16 16:52:53 -0800

    Adding HTTP / HTTPS notes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 $(function(){
-  var p = $.ajax( {
-    url:"http://www.astro-phys.com/api/de406/states?bodies=sun,mercury,venus,earthmoon,geomoon,mars,jupiter,uranus,neptune,pluto",
-    type:"get"});
 
-  p.done(function(data){
+  function redirectToHttp(){
+    window.location.href = "http:" + window.location.href.substring(window.location.protocol.length);
+  }
+
+  if (window.location.protocol == "https:")
+  {
+    var switchToHttpButton = $('<button>');
+    switchToHttpButton.text('This will only only work on HTTP (but why?)')
+    switchToHttpButton.click(redirectToHttp);
+    $('#notice').append(switchToHttpButton);
+  }
+
+  $.ajax( {
+    url:"http://www.astro-phys.com/api/de406/states?bodies=sun,mercury,venus,earthmoon,geomoon,mars,jupiter,uranus,neptune,pluto",
+    type:"get"}).done(function(data){
       $('#answer').text(JSON.stringify(data.results,null,2));
     });
 });
